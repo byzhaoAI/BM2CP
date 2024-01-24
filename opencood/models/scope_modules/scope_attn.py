a00c1e1b097537f372c69d937fd5ee727471f8fe
byzhao <binyu-zhao@outlook.com>
2024-01-24 13:35:36 +0000

    up

--- opencood/models/scope_modules/scope_attn.py
+++ opencood/models/scope_modules/scope_attn.py
@@ -183,7 +183,6 @@ class Communication(nn.Module):
         
         self.smooth = False
         self.thre = args['thre']
-        self.compressed_dim = args['compressed_dim']
         if 'gaussian_smooth' in args:
             self.smooth = True
             kernel_size = args['gaussian_smooth']['k_size']
@@ -202,39 +201,37 @@ class Communication(nn.Module):
         self.gaussian_filter.weight.data = torch.Tensor(gaussian_kernel).to(self.gaussian_filter.weight.device).unsqueeze(0).unsqueeze(0)
         self.gaussian_filter.bias.data.zero_()
 
-    def forward(self, psm):
-        B = len(psm)
-        _, _, H, W = psm[0].shape
+    def forward(self, batch_confidence_maps, record_len, pairwise_t_matrix):
+        B, L, _, _, _ = pairwise_t_matrix.shape 
+        _, _, H, W = batch_confidence_maps[0].shape 
         
-
-        private_confidence_maps = []
-        private_communication_masks = []
+        communication_masks = []
         communication_rates = []
-        
+        batch_communication_maps = []
+        deform_communication_map = []
         for b in range(B):
-            ori_private_communication_maps = psm[b].sigmoid().max(dim=1)[0].unsqueeze(1)  
+            N = record_len[b]
+            ori_communication_maps = batch_confidence_maps[b].sigmoid().max(dim=1)[0].unsqueeze(1) # dim1=2 represents the confidence of two anchors
             
             if self.smooth:
-                private_communication_maps = self.gaussian_filter(ori_private_communication_maps)
+                communication_maps = self.gaussian_filter(ori_communication_maps)
+                deform_communication_map.append(communication_maps)
             else:
-                private_communication_maps = ori_private_communication_maps
-            private_confidence_maps.append(private_communication_maps)  
-                
+                communication_maps = ori_communication_maps
 
-            ones_mask = torch.ones_like(private_communication_maps).to(private_communication_maps.device)
-            zeros_mask = torch.zeros_like(private_communication_maps).to(private_communication_maps)
+            ones_mask = torch.ones_like(communication_maps).to(communication_maps.device)
+            zeros_mask = torch.zeros_like(communication_maps).to(communication_maps.device)
+            communication_mask = torch.where(communication_maps>self.thre, ones_mask, zeros_mask)
+
+            communication_rate = communication_mask[0].sum()/(H*W)
             
-            private_mask = torch.where(private_communication_maps > self.thre, ones_mask, zeros_mask)  
-            cav_num = private_mask.shape[0]
-            private_rate = private_mask[1:].sum()/((cav_num-1) * H * W)
-            
-            private_mask_nodiag = private_mask.clone()
-            ones_mask = torch.ones_like(private_mask).to(private_mask.device)
-            private_mask_nodiag[::2] = ones_mask[::2]  
-            private_communication_masks.append(private_mask_nodiag)
-            communication_rates.append(private_rate)
-            
+            communication_mask_nodiag = communication_mask.clone()
+            ones_mask = torch.ones_like(communication_mask).to(communication_mask.device)
+            communication_mask_nodiag[::2] = ones_mask[::2]
+
+            communication_masks.append(communication_mask_nodiag)
+            communication_rates.append(communication_rate)
+            batch_communication_maps.append(ori_communication_maps*communication_mask_nodiag)
         communication_rates = sum(communication_rates)/B
-        private_mask = torch.cat(private_communication_masks, dim=0)  
-        
-        return private_mask, communication_rates, private_confidence_maps
+        communication_masks = torch.cat(communication_masks, dim=0)
+        return batch_communication_maps, communication_masks, communication_rates, deform_communication_map
