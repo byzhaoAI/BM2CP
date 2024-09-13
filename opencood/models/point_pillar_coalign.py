921c51d94fc6ee177e1782421cca5d5ef6461ae8
byzhao <binyu-zhao@outlook.com>
2024-09-13 16:17:57 +0000

    coalign name

--- opencood/models/point_pillar_coalign.py
+++ opencood/models/point_pillar_coalign.py
@@ -4,5 +4,5 @@ CoAlign Fusion Network is multiscale att fusion with ResNet Backbone
 
 from opencood.models.point_pillar_baseline_multiscale import PointPillarBaselineMultiscale
 
-class CoAlign(PointPillarBaselineMultiscale):
+class PointPillarCoAlign(PointPillarBaselineMultiscale):
     pass
