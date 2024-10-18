cb5fb08556503f33434c43a6b491c6d1104ca4f2
byzhao <binyu-zhao@outlook.com>
2024-10-18 03:45:06 +0000

    add segmentation

---
+++ opencood/loss/combined_loss.py
@@ -0,0 +1,84 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from importlib import import_module
+
+
+class CombinedLoss(nn.Module):
+    def __init__(self, args):
+        super(CombinedLoss, self).__init__()
+        self.losses = []
+        for k, v in args.items():
+            module = import_module(f"opencood.loss.{k}")
+            loss_name = ''.join([x[0].upper() + x[1:] for x in k.split('_')])
+            self.losses.append(getattr(module, loss_name)(v))
+        self.loss_dict = {}
+
+    def forward(self, output_dict, target_dict):
+        """
+        Parameters
+        ----------
+        output_dict : dict
+        target_dict : dict
+        """
+        total_loss = 0
+        for loss in self.losses:
+            total_loss += loss(output_dict, target_dict)
+            self.loss_dict.update(loss.loss_dict)
+        self.loss_dict['total_loss'] = total_loss
+
+        return total_loss
+
+    def logging(self, epoch, batch_id, batch_len, writer, pbar=None):
+        """
+        Print out  the loss function for current iteration.
+
+        Parameters
+        ----------
+        epoch : int
+            Current epoch for training.
+        batch_id : int
+            The current batch.
+        batch_len : int
+            Total batch length in one iteration of training,
+        writer : SummaryWriter
+            Used to visualize on tensorboard
+        """
+        total_loss = self.loss_dict['total_loss']
+        reg_loss = self.loss_dict['reg_loss']
+        conf_loss = self.loss_dict['conf_loss']
+        dynamic_loss = self.loss_dict['dynamic_loss']
+        static_loss = self.loss_dict.get('static_loss', None)
+        if static_loss is None:
+            static_loss = torch.tensor(0.0)
+        if pbar is None:
+            print("[epoch %d][%d/%d], || Loss: %.4f "
+                  "|| Dynamic Loss: %.4f || Static Loss: %.4f "
+                  "|| Conf Loss: %.4f"
+                " || Loc Loss: %.4f" % (
+                    epoch, batch_id + 1, batch_len, total_loss.item(),
+                    dynamic_loss.item(), static_loss.item(),
+                    conf_loss.item(), reg_loss.item()))
+        else:
+            pbar.set_description("[epoch %d][%d/%d], || Loss: %.4f "
+                                 "|| Dynamic Loss: %.4f "
+                                 "|| Static Loss: %.4f "
+                                 "|| Conf Loss: %.4f"
+                                 " || Loc Loss: %.4f" % (
+                      epoch, batch_id + 1, batch_len, total_loss.item(),
+                      dynamic_loss.item(), static_loss.item(),
+                      conf_loss.item(), reg_loss.item()))
+
+
+        writer.add_scalar('Regression_loss', reg_loss.item(),
+                          epoch*batch_len + batch_id)
+        writer.add_scalar('Confidence_loss', conf_loss.item(),
+                          epoch*batch_len + batch_id)
+        writer.add_scalar('Dynamic_loss', dynamic_loss.item(),
+                          epoch*batch_len + batch_id)
+        if static_loss > 0:
+            writer.add_scalar('Static_loss', static_loss.item(),
+                              epoch*batch_len + batch_id)
+        writer.add_scalar('Total_loss', total_loss.item(),
+                          epoch*batch_len + batch_id)
---
+++ opencood/loss/point_pillar_seg_loss.py
@@ -0,0 +1,85 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+from einops import rearrange
+
+
+class PointPillarSegLoss(nn.Module):
+    def __init__(self, args):
+        super(PointPillarSegLoss, self).__init__()
+        self.l_weights = 50 if 'l_weights' not in args else args['l_weights']
+
+        self.d_weights = args['d_weights']
+        self.d_coe = args['d_coe']
+        self.loss_func_dynamic = \
+            nn.CrossEntropyLoss(
+                weight=torch.Tensor([1., self.d_weights]).cuda())
+
+        self.loss_dict = {}
+
+    def forward(self, output_dict, gt_dict):
+        """
+        Perform loss function on the prediction.
+
+        Parameters
+        ----------
+        output_dict : dict
+            The dictionary contains the prediction.
+
+        gt_dict : dict
+            The dictionary contains the groundtruth.
+
+        Returns
+        -------
+        Loss dictionary.
+        """
+        total_loss = 0
+        
+        dynamic_pred = output_dict['seg_preds']
+        # during training, we only need to compute the ego vehicle's gt loss
+        dynamic_gt = (gt_dict['gt_dynamic'] > 0.5).long()
+        
+        dynamic_loss = self.loss_func_dynamic(dynamic_pred, dynamic_gt)
+        total_loss += self.d_coe * dynamic_loss
+        self.loss_dict.update({'dynamic_loss': dynamic_loss})
+
+        self.loss_dict.update({'total_loss': total_loss})
+
+        return total_loss
+
+    def logging(self, epoch, batch_id, batch_len, writer, pbar=None):
+        """
+        Print out  the loss function for current iteration.
+
+        Parameters
+        ----------
+        epoch : int
+            Current epoch for training.
+        batch_id : int
+            The current batch.
+        batch_len : int
+            Total batch length in one iteration of training,
+        writer : SummaryWriter
+            Used to visualize on tensorboard
+        """
+        total_loss = self.loss_dict['total_loss']
+        static_loss = self.loss_dict['static_loss']
+        dynamic_loss = self.loss_dict['dynamic_loss']
+
+        if pbar is None:
+            print("[epoch %d][%d/%d], || Loss: %.4f || static Loss: %.4f"
+                " || Dynamic Loss: %.4f" % (
+                    epoch, batch_id + 1, batch_len,
+                    total_loss.item(), static_loss.item(), dynamic_loss.item()))
+        else:
+            pbar.set_description("[epoch %d][%d/%d], || Loss: %.4f || static Loss: %.4f"
+                  " || Dynamic Loss: %.4f" % (
+                      epoch, batch_id + 1, batch_len,
+                      total_loss.item(), static_loss.item(), dynamic_loss.item()))
+
+        if writer is not None:
+            writer.add_scalar('Static_loss', static_loss.item(),
+                              epoch*batch_len + batch_id)
+            writer.add_scalar('Dynamic_loss', dynamic_loss.item(),
+                              epoch*batch_len + batch_id)
